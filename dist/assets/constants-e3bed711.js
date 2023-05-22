9f42ca0cb5abd729ddcb7ab6b1e5242117b7bf32
Edcreation <93517096+Edcreation@users.noreply.github.com>
2023-05-22 23:44:58 +0200

    deployment
    Deployed to git

---
+++ dist/assets/constants-e3bed711.js
@@ -0,0 +1,2 @@
+const t=[{text:"Home",value:""},{text:"Browse",value:"browse"},{text:"Support",value:"support"},{text:"Contact Us",value:"contacts"}],o=` Protecting your account is our top priority. Please confirm your
+account by entering the authorization code sent to`;export{t as n,o as t};
